79664b7a533c9bab2438e3e24e03dcc41f830281
zhouzhengfw <zheng@fireworkhq.com>
2023-01-31 21:36:19 +0800

    modify plugin

--- fw-initializer/src/main/groovy/com/loopnow/gradle/CameraInitPlugin.groovy
+++ fw-initializer/src/main/groovy/com/loopnow/gradle/CameraInitPlugin.groovy
@@ -13,19 +13,23 @@ class CameraInitPlugin implements Plugin<PluginAware> {
 
          if (target instanceof Project) {
             println "初始化FW Camera"
-            def fwLibraryVersion = "1.4.3-test"
+            def fwLibraryVersion = "1.4.3.1-test"
             if (target.getPlugins().hasPlugin(AppPlugin.class)){
                 BaseExtension android = target.extensions.getByName("android") as BaseExtension
                 android.compileOptions.sourceCompatibility = JavaVersion.VERSION_1_8
                 android.compileOptions.targetCompatibility = JavaVersion.VERSION_1_8
                 android.defaultConfig.javaCompileOptions.annotationProcessorOptions.arguments.put "AROUTER_MODULE_NAME", target.getName()
+                android.defaultConfig.addManifestPlaceholders(Map.of("envPermission",android.defaultConfig.getApplicationId()) )
+                android.defaultConfig.addManifestPlaceholders(Map.of("envProviderFilter",android.defaultConfig.getApplicationId()) )
+//                manifestPlaceholders["envPermission"] = "com.firework.sdkdemo"
+//                manifestPlaceholders["envProviderFilter"] = "com.firework.sdkdemo"
                 target.dependencies.add('implementation', "com.loopnow.library:camera:$fwLibraryVersion")
-                target.dependencies.add('implementation', "com.loopnow.library:auth:$fwLibraryVersion")
-                target.dependencies.add('implementation', "com.loopnow.library.base:base-ui:$fwLibraryVersion")
-                target.dependencies.add('implementation', "com.loopnow.library.base:network:$fwLibraryVersion")
-                target.dependencies.add('implementation', "com.loopnow.library.base:util:$fwLibraryVersion")
-                target.dependencies.add('implementation', "com.loopnow.library:camera-framework:$fwLibraryVersion")
-                target.dependencies.add('implementation', "com.loopnow.library:content-management:$fwLibraryVersion")
+//                target.dependencies.add('implementation', "com.loopnow.library:auth:$fwLibraryVersion")
+//                target.dependencies.add('implementation', "com.loopnow.library.base:base-ui:$fwLibraryVersion")
+//                target.dependencies.add('implementation', "com.loopnow.library.base:network:$fwLibraryVersion")
+//                target.dependencies.add('implementation', "com.loopnow.library.base:util:$fwLibraryVersion")
+//                target.dependencies.add('implementation', "com.loopnow.library:camera-framework:$fwLibraryVersion")
+//                target.dependencies.add('implementation', "com.loopnow.library:content-management:$fwLibraryVersion")
                 target.dependencies.add('implementation', "com.loopnow.library:live-stream-kit:$fwLibraryVersion")
                 println "Init FW Camera Success"
             }
